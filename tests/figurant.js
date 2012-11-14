0edd667d4604d00e0b27a4a669c3f099719357e9
Johannes Hofmeister <cessor@github.com>
2012-11-14 19:19:16 +0100

    We can now anticipate functions to be returned. Isnt that something?

--- tests/figurant.js
+++ tests/figurant.js
@@ -1,5 +1,6 @@
 var iAnticipateThat = require('../lib/index');
 var compare = require('compare.js');
+var _ = require('underscore');
 var util = require('util');
 
 var give = function (value) {
@@ -9,24 +10,14 @@ var give = function (value) {
 	return fn;
 }
 
-var logArgs = function (args) {
-	for(var a in args){
-		console.log('\t' + args[a]);
-	}
-};
-
 var take = function () {
-	var anticipatedInput = arguments;
-	console.log('outer args: ');
-	logArgs(anticipatedInput);
+	var anticipated = _.flatten(arguments);
 	return function () { 
-		var ai = arguments;
-		console.log('inner args: ');
-		logArgs(ai);
-		if(ai !== anticipatedInput) {
-			// JH, 12.11.2012 - This should actually use the 'unanticipated' function from the mothership (the index module)
-			throw new Error(util.format('Did not take %s', anticipatedInput));
+		var actual = _.flatten(arguments);
+		if(compare.eq(actual, anticipated)) {
+			return;
 		}
+		throw new Error(util.format('Did not take %s', anticipated)); // JH, 12.11.2012 - This should actually use the 'unanticipated' function from the mothership (the index module)
 	};
 };
 
@@ -36,34 +27,49 @@ var right = 5;
 var wrong = 10;
 var thisIsWhatIWant = 5;
 
-var callWithWrongInput = function (fn) {
-	fn(wrong);
-}; 
-
 var callWithRightInput = function (fn) {
 	fn(right);
-}; 
+};
+
+var callWithWrongInput = function (fn) {
+	fn(wrong);
+};
 
 var callIt = function (fn) {
 	return fn();
 };
+
+var anyFunction = function() {
+	return function() { };
+};
+
+// kill this
+var logArgs = function (args) {
+	for(var a in args){
+		console.log('\t' + args[a]);
+	}
+};
+
+iAnticipateThat(give, {
+	'gives me a function, that returns the input of give' : { in: 5, out: anyFunction }
+});
 
 iAnticipateThat(callIt, {
 	'gives me what I put in as indirect input' : {in: give(5), out: 5}
 });
 
 iAnticipateThat(callWithRightInput, { 
-	'makes the take function happy.': { in: take(thisIsWhatIWant) }
-});  	
+	'makes the take function happy.': [ 
+		{ in: take(thisIsWhatIWant) } ,
+		{ in: take([1,2,3]) } ,
+	]
+});
 
 iAnticipateThat(callWithWrongInput, { 
 	'makes the take function unhappy.': { in: take(thisIsWhatIWant), error: { message: 'Did not take 5' } }
 });
 
-console.log('compare');
-iAnticipateThat(compare.eqs, {
-	'can compare two arrays in a sequence' : [
-		{ in: [[1,2,3],[1,2,3]], out:true },
-		{ in: [{m:5},{m:5}], out:true }
-	]
+iAnticipateThat(compare.eq, {
+	'can compare two arrays in a sequence' : { in: [[1,2,3],[1,2,3]], out:true },
+	'can compare two objects' :	{ in: [{m:5},{m:5}], out:true }
 });
